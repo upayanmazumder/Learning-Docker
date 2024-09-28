1fbcc046cc8c6ba63e3a7b2d3c51fd68b4360dd4
upayanmazumder <upayanm3@gmail.com>
2024-09-29 00:43:23 +0530

    after nohup, this

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from my VPS! Did docker! Also setup webhook! uhh');
+  res.send('Hello from my VPS! Did docker! Also setup webhook! Also did the nohup thing');
 });
 
 app.listen(port, () => {
