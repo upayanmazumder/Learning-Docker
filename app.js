3749d14d997a5f8a50d1c4231cbfbb09f72f1396
upayanmazumder <upayanm3@gmail.com>
2024-09-28 22:36:16 +0530

    updates

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // app.js
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5100;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello from my VPS! Did docker');
