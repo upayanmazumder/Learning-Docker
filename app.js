2e29472045cb694df2db2829435b05fb4fc59d72
upayanmazumder <upayanm3@gmail.com>
2024-09-29 11:31:32 +0530

    added script. lets see

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from my VPS! Did docker! Also setup webhook! Also did the nohup thing');
+  res.send('Hello from my VPS! Did docker! Also setup webhook! Also did the nohup thing! I have setup monitor_docker script.');
 });
 
 app.listen(port, () => {
