7dd871ac052bad7d4aa74f0d966d7f7c40590c89
upayanmazumder <upayanm3@gmail.com>
2024-09-29 14:24:51 +0530

    screen 1

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('gggg ggg');
+  res.send('now running on screen. 1');
 });
 
 app.listen(port, () => {
