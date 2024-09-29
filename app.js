1359478ae3c30b0b86208c341fe5d58afa14ef19
upayanmazumder <upayanm3@gmail.com>
2024-09-29 13:35:02 +0530

    gg

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('LETS FUCKING GOOOO!');
+  res.send('ok this works');
 });
 
 app.listen(port, () => {
