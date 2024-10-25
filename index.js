c95745f7f0a78fea1ef122cf0d1356caf321cfa8
upayanmazumder <upayanm3@gmail.com>
2024-10-25 20:35:02 +0530

    Update docker-compose.yml to add Caddy reverse proxy
    - Added a new service 'caddy' in docker-compose.yml
    - Configured Caddy to act as a reverse proxy from 'https://bots.upayan.space' to 'http://backend:3000'
    - Exposed ports 443 and 80 for the Caddy service
    - No changes made to the 'backend' service
    
    Update index.js to change the response message
    
    - Modified the response message for the root route from 'Hello, Mars!' to 'Hey what are you doing trying to access my VPS!'

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Mars!');
+  res.send('Hey what are you doing trying to access my VPS!');
 });
 
 app.listen(port, () => {
